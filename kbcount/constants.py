fb377c8b431ca51c085a7b74b3950edb6889b182
Lioscro <kmin@caltech.edu>
2019-10-15 10:26:39 -0700

    update constants

--- kbcount/constants.py
+++ kbcount/constants.py
@@ -6,3 +6,15 @@ MINIMUM_REQUIREMENTS = {
 
 INDEX_FILENAME = 'transcriptome.idx'
 REFERENCE_FILENAME = 'transcriptome.fasta'
+
+BUS_FILENAME = 'output.bus'
+BUS_S_FILENAME = 'output.s.bus'
+BUS_SC_FILENAME = 'output.s.c.bus'
+BUS_SCS_FILENAME = 'output.s.c.s.bus'
+
+GENEMAP_FILENAME = 'transcripts_to_genes.txt'
+ECMAP_FILENAME = 'matrix.ec'
+TXNAMES_FILENAME = 'transcripts.txt'
+
+COUNTS_DIR = 'counts'
+COUNTS_FILENAME = 'genes'
