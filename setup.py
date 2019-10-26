9801aea802cdbbe5e108b01707186a8385151692
Lioscro <kmin@caltech.edu>
2019-10-26 10:37:20 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = read('README.md')
 
 setup(
     name='kb_python',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/pachterlab/kb_python',
     author='Kyung Hoi (Joseph) Min',
     author_email='phoenixter96@gmail.com',
