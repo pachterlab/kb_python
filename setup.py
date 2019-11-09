b0f9ff6b3faf74af6e50fb16415a04709fa12702
Lioscro <kmin@caltech.edu>
2019-11-09 10:34:02 -0800

    add python 3.8 to setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Utilities',
     ],
